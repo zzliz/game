afcf62f67c7a2cfc9f3246dfa4bc5299681aaaea
zzliz <lbenitez4@csuchico.edu>
2024-07-11 02:17:29 +0000

    change 3

--- script/script.js
+++ script/script.js
@@ -1,23 +1,8 @@
-var character = document.getElementById("character");
-var block = document.getElementById("block");
-var counter=0;
-function jump(){
-    if(character.classList == "animate"){return}
-    character.classList.add("animate");
-    setTimeout(function(){
-        character.classList.remove("animate");
-    },300);
-}
-var checkDead = setInterval(function() {
-    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
-    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
-    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
-        block.style.animation = "none";
-        alert("Game Over. score: "+Math.floor(counter/100));
-        counter=0;
-        block.style.animation = "block 1s infinite linear";
-    }else{
-        counter++;
-        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
-    }
-}, 10);
+// script.js
+document.addEventListener('DOMContentLoaded', () => {
+    const startButton = document.getElementById('startButton');
+
+    startButton.addEventListener('click', () => {
+        window.location.href = 'https://github.com/username/repository/blob/main/src/index.html'; // Link to your main code or another page
+    });
+})

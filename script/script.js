424c2cae8ba97796b2d48cbccc3bd91cb990fa98
zzliz <lbenitez4@csuchico.edu>
2024-07-11 02:14:58 +0000

    Add landing page with Start Button

--- script/script.js
+++ script/script.js
@@ -1,15 +1,23 @@
-function preload(){
-    /* put stuff that need to be loaded before the canvas gets rendered here */
+var character = document.getElementById("character");
+var block = document.getElementById("block");
+var counter=0;
+function jump(){
+    if(character.classList == "animate"){return}
+    character.classList.add("animate");
+    setTimeout(function(){
+        character.classList.remove("animate");
+    },300);
 }
-
-function setup(){
-    /* put stuff that need to be setup before the draw loop here */
-    createCanvas(400, 400);
-}
-
-function draw(){
-    /* put stuff that need to be drawn here animaton happens here */
-    background(0);
-    fill(255);
-    ellipse(mouseX, mouseY, 50, 50);
-}
+var checkDead = setInterval(function() {
+    let characterTop = parseInt(window.getComputedStyle(character).getPropertyValue("top"));
+    let blockLeft = parseInt(window.getComputedStyle(block).getPropertyValue("left"));
+    if(blockLeft<20 && blockLeft>-20 && characterTop>=130){
+        block.style.animation = "none";
+        alert("Game Over. score: "+Math.floor(counter/100));
+        counter=0;
+        block.style.animation = "block 1s infinite linear";
+    }else{
+        counter++;
+        document.getElementById("scoreSpan").innerHTML = Math.floor(counter/100);
+    }
+}, 10);
